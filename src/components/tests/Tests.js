cac41b63c26087a32767d9d6eecedb6b844ff82c
johnthomsonn <johnthomson16@hotmail.co.uk>
2022-12-03 12:13:14 +0000

    tests-explorer

--- src/components/tests/Tests.js
+++ src/components/tests/Tests.js
@@ -1,22 +1,60 @@
-import React, {useState} from "react"
-
+import React, {useState, useEffect} from "react"
+import "./Tests.css"
+const {ipcRenderer,shell} = window.require('electron')
 
 const Tests = props => {
 
     const [globalParam,setGlobalParam] = useState(null)
     const [activeTest,setActiveTest] = useState(null)
+    const [fileStructure,setFileStructure] = useState(null)
+    const [rootPath,setRootPath] = useState(null)
 
+    useEffect(() => {
+        getFilesAndFoldersForTestReports() 
+        return () => unmount()
+    },[])
+
+    const getFilesAndFoldersForTestReports = () => {
+        ipcRenderer.send("get-tests-explorer")
+    }
+
+    const clickPathItem = (pathId,path) => {
+        setActiveTest({
+            path,
+            pathId
+        })
+        document.getElementById(activeTest.pathId).classList.remove('active-path-item')
+        document.getElementById(pathId).classList.add('active-path-item')
+    }
+
+    const unmount = () => {
+        setActiveTest(null)
+    }
+
+    ipcRenderer.on('tests-explorer', (event,structure,rootPath) => {
+        setFileStructure(structure)
+        setRootPath(rootPath)
+    }) 
+
+    const displayExplorer = () => {
+        if(!fileStructure) return
+
+        return fileStructure.map((path,i) => {
+            const pathId = path.replace(rootPath+"\\","")
+            return <div id={pathId} className="path-item" onClick={() => clickPathItem(pathId,path)}>{pathId}</div>
+        })
+    }
 
     return (<>
     
         <div className="test-reports-container">
             <div className="test-reports-explorer-container">
                 <div className="test-reports-explorer">
-                Explorer window
+                    {displayExplorer()}
                 </div>
                 <div className="test-reports-explorer-buttons">
-                    <button>Set global param</button>
-                    <button>Set active test</button>
+                    <button className="set-global-param-btn">Set global param</button>
+                    <button className="set-active-test-btn">Run Test</button>
                 </div>
             </div>
             
